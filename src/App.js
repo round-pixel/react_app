a35f79b5c1ae658e0e837234a27785622838660c
Alexey Ivanov <Aleksey.Ivanov@apluss.ru>
2017-11-28 19:57:28 +0300

    Install redux, react-redux

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { Router } from 'react-router-dom';
 
 import history from 'helpers/history';
 
+import { Provider } from 'react-redux';
+
+import store from 'store';
+
 import MainLayout from 'components/layouts/MainLayout';
 
 const App = () => (
-  <Router history={history}>
-    <MainLayout />
-  </Router>
+  <Provider store={store} >
+    <Router history={history}>
+      <MainLayout />
+    </Router>
+  </Provider>
 );
 
 export default App;
--- src/constants/url.js
+++
@@ -1,2 +0,0 @@
-export const postsUrl = 'http://localhost:3002/';
-export const postUrl = 'http://localhost:3002/';
