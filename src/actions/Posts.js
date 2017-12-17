b0b60097746c22d19a3616b5101e5cb6db0108f2
round-pixel <starmuratovo@gmail.com>
2017-12-17 11:28:15 +0300

    Recieve data from custom middleware

--- src/actions/Posts.js
+++ src/actions/Posts.js
@@ -1,28 +1,17 @@
-import request from 'superagent';
-
-import { API_ROOT } from 'constants/API';
 import * as types from 'constants/actionTypes/PostsActionTypes';
-
-const requestPosts = () => ({
-  type: types.FETCH_POSTS_REQUEST
-});
-
-const receivePosts = (response) => ({
-  type: types.FETCH_POSTS_SUCCESS,
-  response
-});
-
-const errorPosts = () => ({
-  type: types.FETCH_POSTS_ERROR
-});
+import { API_CALL } from 'middleware/API';
 
 export function fetchPosts() {
-  return (dispatch) => {
-    dispatch(requestPosts());
-    return request
-      .get(`${API_ROOT}/`)
-      .end((err, response) => {
-        err ? dispatch(errorPosts()) : dispatch(receivePosts(response.body));
-      });
+  return {
+    [API_CALL]: {
+      endpoint: '/',
+      method: 'GET',
+      query: {},
+      types: [
+        types.FETCH_POSTS_REQUEST,
+        types.FETCH_POSTS_SUCCESS,
+        types.FETCH_POSTS_ERROR
+      ]
+    }
   };
 }
