4f18ee9ab0b60f1f7c041603986a09c5435dfa15
Alexey Ivanov <Aleksey.Ivanov@apluss.ru>
2017-12-19 21:14:10 +0300

    Render like on posts page. Fix search

--- src/reducers/Posts.js
+++ src/reducers/Posts.js
@@ -1,11 +1,24 @@
 import { assign } from 'lodash/object';
 import * as types from 'constants/actionTypes/PostsActionTypes';
+import * as likeTypes from 'constants/actionTypes/AddLike';
+import update from 'immutability-helper';
 
 const initialState = {
   isFetching: false,
   error: false,
   entries: []
 };
+
+function addLike(posts, id, count) {
+  if (posts) {
+    const index = posts.findIndex((post) => post.id == id);
+    return update(
+      posts,
+      { [index]: { likes: { $apply: () => count } } }
+    );
+  }
+  return posts;
+}
 
 export default function(state = initialState, action) {
   switch (action.type) {
@@ -15,6 +28,11 @@ export default function(state = initialState, action) {
       return assign({}, initialState, { error: true });
     case types.FETCH_POSTS_SUCCESS:
       return assign({}, initialState, { entries: action.response });
+    case likeTypes.ADD_LIKE: {
+      const id = action.response.id;
+      const count = action.response.likes;
+      return assign({}, state, { entries: addLike(state.entries, id, count) });
+    }
     default:
       return state;
   }
