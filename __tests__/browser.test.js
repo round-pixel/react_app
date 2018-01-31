23a59be3966a95a850428d0ae2c4e4c8a9a190ab
round-pixel <starmuratovo@gmail.com>
2018-02-01 02:27:21 +0300

    Test increase likes

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -1,4 +1,5 @@
 import nightmare from 'nightmare';
+jest.setTimeout(10000);
 
 describe('When visit the homepage', () => {
   it('welcomes the user', async () => {
@@ -6,5 +7,17 @@ describe('When visit the homepage', () => {
     let text = await page.evaluate(() => (document.body.textContent)).end();
 
     expect(text).toContain('Nature');
+  });
+
+  it('increase like count', async () => {
+    let page = nightmare().goto('http://localhost:3001/posts/1');
+    let oldCount = await page.evaluate(() => (document.querySelector('.likeCount').innerText)).end();
+
+    page.click('.addLike');
+
+    page.evaluate(() => (document.querySelector('.likeCount').innerText))
+      .then((newCount) => {
+        expect(newCount).toEqual(oldCount + 1);
+      });
   });
 });
