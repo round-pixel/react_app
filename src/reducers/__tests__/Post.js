bddfc17945d82f64d9b326ab678dc76fd7ddd679
round-pixel <starmuratovo@gmail.com>
2018-02-01 01:02:30 +0300

    Post reducer test

---
+++ src/reducers/__tests__/Post.js
@@ -0,0 +1,42 @@
+import reducer from 'reducers/Post';
+import * as types from 'constants/actionTypes/PostActionTypes';
+
+describe('post reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(
+      {
+        isFetching: false,
+        error: false,
+        entry: null
+      }
+    );
+  });
+
+  it('should fetch post request', () => {
+    expect(
+      reducer([], {
+        type: types.FETCH_POST_REQUEST
+      })
+    ).toEqual(
+      {
+        isFetching: true,
+        error: false,
+        entry: null
+      }
+    );
+  });
+
+  it('should return error', () => {
+    expect(
+      reducer([], {
+        type: types.FETCH_POST_ERROR,
+      })
+    ).toEqual(
+      {
+        isFetching: false,
+        error: true,
+        entry: null
+      }
+    );
+  });
+});
